f1848586e66b9454d9bb171ccaadc3e06b8b137f
Arthur Ribeiro <148250310+arthurrsn@users.noreply.github.com>
2024-06-24 12:06:47 -0300

    Create app.js

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert('Seja bem vindo(a) ao NUMBER SECRET');
+let Secretnumber = 7;
+let pergunta = prompt('Digite um número de 1 a 10:')
+
+if (pergunta == Secretnumber) {
+    console.log('Acertou!')
+}                                    
