40dbc7003305a0bb9eec04a87cdc36ba3d7acde9
Arthur Ribeiro <148250310+arthurrsn@users.noreply.github.com>
2024-06-24 23:29:21 -0300

    Day 001

---
+++ iniciando com JS copy/Desafio_001.js
@@ -0,0 +1,17 @@
+alert('Boas vindas ao nosso site!');
+
+let nome = 'lua';
+let idade = 25;
+let numeroDeVenda = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+let Nome = prompt('Digite o seu nome:');
+let Idade = prompt('Digite sua idade:');
+
+if (Idade >= 18) {
+    alert('Pode tirar a habilitação!')
+}
